e102e6b1bb2a000a02c0c184e7f705564ee19df9
deepitha0011 <84723096+deepitha0011@users.noreply.github.com>
2021-07-02 14:54:11 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,25 @@
+void swap_max (int arr[],int l,int n)
+{
+  int max=0;
+  int index=n;
+  for (int i=n;i<l;i++)
+  {
+    if (max<arr[i])
+    {
+      max=arr[i];
+      index=i;
+    }
+  }
+int temp=0;
+temp=arr[n];
+arr[n]=arr[index];
+arr[index]=temp;
+}
+
+void ssort (int arr[],int i)
+{
+  for(int i=0;intl;inti)
+  {
+    swap_max (arr[],l,i);
+  }
+}
